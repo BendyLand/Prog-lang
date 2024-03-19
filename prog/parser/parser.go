fc761a28141b11387e9c5a3a2377b36b803e568c
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-19 18:23:39 -0400

    Start new direction of parser

--- prog/parser/parser.go
+++ prog/parser/parser.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
-	"parser/keywords"
+	// "parser/keywords"
 	// "parser/expressions"
 )
 
@@ -18,91 +18,6 @@ func main() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-func matchKeywordsToTokens(keywords []string, tokens [][]string) map[string][]string {
-	resultMap := make(map[string][]string, len(keywords))
-	var filteredTokens [][]string
-	for _, keyword := range keywords {
-		temp := filterContainsKeyword(tokens, keyword)
-		filteredTokens = append(filteredTokens, temp)
-	}
-	for i := 0; i < len(keywords); i++ {
-		resultMap[keywords[i]] = filteredTokens[i]
-	}
-	return resultMap
-}
-
-func filterContainsKeyword(tokens [][]string, keyword string) []string {
-	var newSlice []string
-	for _, line := range tokens {
-		for _, word := range line {
-			if word == keyword {
-				newLine := strings.Join(line, " ")
-				newSlice = append(newSlice, newLine)
-			}
-		}
-	}
-	return newSlice
-}
-
-
-func extractKeywords(tokens [][]string) []string {
-	var foundKeywords []string
-	for _, line := range tokens {
-		for _, token := range line {
-			if keywords.Keywords.Contains(token) {
-				foundKeywords = append(foundKeywords, token)
-			}
-		}
-	}
-	return foundKeywords
-}
-
-func processLinesIntoTokens(lines []string) [][]string {
-	var allTokens [][]string
-	for _, line := range lines {
-		tokens := splitIntoTokens(line)
-		allTokens = append(allTokens, tokens)
-	}
-	return allTokens
-}
-
-func splitIntoTokens(line string) []string {
-	tokens := strings.Split(line, " ")
-	return tokens
-}
 
 func splitIntoChars(file string) []string {
 	lines := strings.Split(file, "")
