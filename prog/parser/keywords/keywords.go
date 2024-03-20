9cab9b78aa677ded30546ed246b05de9e06f9691
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-19 20:43:07 -0400

    Create formatTokens and helper functions

--- prog/parser/keywords/keywords.go
+++ prog/parser/keywords/keywords.go
@@ -2,9 +2,17 @@ package keywords
 
 type StringList []string
 
-var Keywords = StringList{"if", "for", "elif", "else", "while", "let", "const", "print", "puts"}
+var Keywords = StringList{"if", "for", "elif", "else", "while", "let", "const", "print", "puts", "let", "mut"}
 
-func (l StringList) Contains(word string) bool {
+func CheckAgainstKeywords(word string) bool {
+	return Keywords.contains(word)
+}
+
+func IsComment(token string) bool {
+	return token == "//"
+}
+
+func (l StringList) contains(word string) bool {
 	for _, item := range l {
 		if item == word {
 			return true
--- prog/parser/parser.go
+++ prog/parser/parser.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"parser/keywords"
 	"strings"
 	// "parser/keywords"
 	// "parser/expressions"
@@ -10,9 +11,57 @@ import (
 
 func main() {
 	file := readFile("../../test.pr")
+	file = removeComments(file)
 	oneLiner := removeNewlines(file)
-	chars := formatString(oneLiner)
-	fmt.Println(chars)
+	withSpaces := formatString(oneLiner)
+	tokens := constructTokens(withSpaces)
+	test := formatTokens(tokens)
+	fmt.Println(test)
+}
+
+func isEmpty(input string) bool {
+	for _, c := range input {
+		if c != ' ' && c != '\t' {
+			return false
+		}
+	}
+	return true
+}
+
+//! this function wrongly removes actual input as well !!!
+func formatTokens(tokens []string) []string {
+	var newTokens, foundKeywords []string
+	placeholder := ""
+	for _, token := range tokens {
+		if keywords.CheckAgainstKeywords(token) {
+			if len(foundKeywords) > 0 {
+				temp := make([]string, 0)
+				foundKeywords = temp
+				newTokens = append(newTokens, placeholder)
+				placeholder = ""
+			}
+			foundKeywords = append(foundKeywords, token)
+		}
+		if len(foundKeywords) > 0 && !isEmpty(token) {
+			placeholder += token + " "
+		}
+	}
+	return newTokens
+}
+
+func constructTokens(str string) []string {
+	var tokens []string
+	placeholder := ""
+	for i, c := range str {
+		if c == ' ' && str[i-1] == ' ' {
+			tokens = append(tokens, placeholder)
+			placeholder = ""
+		}
+		if c != ' ' {
+			placeholder += string(c)
+		}
+	}
+	return tokens
 }
 
 func removeNewlines(file string) string {
@@ -22,6 +71,17 @@ func removeNewlines(file string) string {
 func formatString(file string) string {
 	chars := strings.Split(file, "")
 	return strings.Join(chars, " ")
+}
+
+func removeComments(file string) string {
+	lines := strings.Split(file, "\n")
+	for i, line := range lines {
+		idx := strings.Index(line, "//")
+		if idx >= 0 {
+			lines[i] = line[:idx]
+		}
+	}
+	return strings.Join(lines, "\n")
 }
 
 func readFile(filename string) string {
